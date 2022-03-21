f6fee215d43c71c53271871f662daa230ef7481d
johnprovazek <johnprovazek@gmail.com>
2022-03-20 17:33:48 -0700

    removing firebase references

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Board from "./components/Board";
 import Console from "./components/Console"
-// import Form from "./components/Form";
-// import TodoList from "./components/TodoList";
-// import firebase from "./util/firebase";
 
 class App extends Component {
     constructor(props) {
@@ -14,40 +11,7 @@ class App extends Component {
             body: "SplashPrints",
             print: ""
         };
-        // this.handleClick = this.handleClick.bind(this);
     }
-
-    // handleClick() {
-    //     console.log("clicked!")
-    //     firebase.auth().signInAnonymously()
-    //     .then(() => {
-    //         // Signed in..
-    //     })
-    //     .catch((error) => {
-    //         var errorCode = error.code;
-    //         var errorMessage = error.message;
-    //         console.log(errorCode)
-    //         console.log(errorMessage)
-    //         // ...
-    //     });
-    // }
-
-    componentDidMount() {
-        // console.log("hello")
-        // firebase.auth().onAuthStateChanged((user) => {
-        //     if (user) {
-        //         // User is signed in, see docs for a list of available properties
-        //         // https://firebase.google.com/docs/reference/js/firebase.User
-        //         var uid = user.uid;
-        //         console.log(uid)
-        //         // ...
-        //         } else {
-        //         // User is signed out
-        //         // ...
-        //     }
-        // });
-    }
-
     render() {
         return (
             <div className="App">
--- client/src/components/Console.js
+++ client/src/components/Console.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import firebase from "../util/firebase";
 
 class Console extends Component {
     constructor(props) {
